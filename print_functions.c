f9f1672fd49bfb53658d0e711b76a50ae20a0a07
hardguyprecious <122944692+hardguyprecious@users.noreply.github.com>
2023-03-25 09:16:56 +0100

    Update print_functions.c

--- print_functions.c
+++ print_functions.c
@@ -7,7 +7,7 @@
  * by Didi and Precious
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculates the active flags
  * @width: Width
  * @precision: Precision specification
  * @size: Size specifier
