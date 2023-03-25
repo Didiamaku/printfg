ffb373b9909c05625e796ed6d906bb6548e63156
Didiamaku <didiamaku@gmail.com>
2023-03-25 02:19:29 -0700

    print

--- handlers.c
+++ handlers.c
@@ -3,7 +3,7 @@
 /************************* WRITE HANDLE *************************/
 /**
  * handle_write_char - Prints a string
- * by Joseph and Ciny
+ * by Didi and Precious
  * @c: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
